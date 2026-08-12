52e42eeb221c036948d206a16710769c4fcab827
Takayuki Miyoshi <takayukister@gmail.com>
2026-08-12 15:20:38 +0900

    Restructure test code
    #13

--- test/index.js
+++ test/index.js
@@ -1,28 +1,42 @@
 import assert from "node:assert/strict";
 import FormDataTree from "../index.js";
 
-// Creating a FormData object for testing.
-const formData = new FormData();
+describe("FormDataTree", function () {
+  // Creating a FormData object for testing.
+  const formData = new FormData();
 
-formData.append("your-name", "John Doe");
-formData.append("your-penguin[]", "Adelie");
-formData.append("your-penguin[]", "Emperor");
-formData.append("your-penguin[]", "Humboldt");
-formData.append("your-file", new Blob(["bla bla bla"]));
+  formData.append("your-name", "John Doe");
+  formData.append("your-penguin[]", "Adelie");
+  formData.append("your-penguin[]", "Emperor");
+  formData.append("your-penguin[]", "Humboldt");
+  formData.append("your-file", new Blob(["bla bla bla"]));
 
-// FormDataTree.from() creates a FormDataTree object from form data.
-const formDataTree = FormDataTree.from(formData);
+  // FormDataTree.from() creates a FormDataTree object from form data.
+  const formDataTree = FormDataTree.from(formData);
 
-// FormDataTree.prototype.getAll() returns an object containing form data values.
-const yourName = formDataTree.getAll("your-name");
+  describe("getAll()", function () {
+    const yourName = formDataTree.getAll("your-name");
 
-assert.deepEqual(yourName, { 0: "John Doe" });
+    it(`should return a single value`, function () {
+      assert.deepEqual(yourName, { 0: "John Doe" });
+    });
 
-const yourPenguins = formDataTree.getAll("your-penguin");
+    const yourPenguins = formDataTree.getAll("your-penguin");
 
-assert.deepEqual(yourPenguins, { 0: "Adelie", 1: "Emperor", 2: "Humboldt" });
+    it(`should return multiple values`, function () {
+      assert.deepEqual(yourPenguins, {
+        0: "Adelie",
+        1: "Emperor",
+        2: "Humboldt",
+      });
+    });
+  });
 
-// For file fields, use FormDataTree.prototype.getAllFiles().
-const yourFile = formDataTree.getAllFiles("your-file");
+  describe("getAllFiles()", function () {
+    const yourFile = formDataTree.getAllFiles("your-file");
 
-assert.ok(yourFile[0] instanceof File);
+    it(`should return a File object`, function () {
+      assert(yourFile[0] instanceof File);
+    });
+  });
+});
